16af3ea6bd397cad5205d89d61788d651b0b1888
legop3 <46182676+legop3@users.noreply.github.com>
2026-09-08 18:25:48 -0400

    Start rover service compatibly on old Android

--- app/src/main/java/land/otter/roverd/BootReceiver.kt
+++ app/src/main/java/land/otter/roverd/BootReceiver.kt
@@ -3,11 +3,14 @@ package land.otter.roverd
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            context.startForegroundService(Intent(context, RoverService::class.java))
+            val service = Intent(context, RoverService::class.java)
+            if (Build.VERSION.SDK_INT >= 26) context.startForegroundService(service)
+            else context.startService(service)
         }
     }
 }
