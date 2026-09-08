3f997a5a64f58c6e4cd7091881e6f4dafe81d46a
legop3 <46182676+legop3@users.noreply.github.com>
2026-09-08 17:40:12 -0400

    Build against stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "land.otter.roverd"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "land.otter.roverd"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
